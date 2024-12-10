52bc5742c0ba35af18b20afceacd49b47e12ffe5
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-10 17:02:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        "requests==2.32.0",
+        "requests",
         'peft',
         'torch',
         'diffusers',
