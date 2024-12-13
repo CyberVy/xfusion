ef1bf5d29e0897920f5be1fc8927bd4851a774d4
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-14 00:53:15 +0800

    Update stable_diffusion_enhancement.py

--- xfusion/enhancement/stable_diffusion_enhancement.py
+++ xfusion/enhancement/stable_diffusion_enhancement.py
@@ -91,7 +91,7 @@ def generate_image_and_send_to_telegram(pipeline,prompt,negative_prompt,num,seed
         image = pipeline(**kwargs).images[0] if use_enhancer else pipeline.__oins__(**kwargs).images[0]
         images.append(image)
         caption = f"Prompt: {prompt[:384]}\n\nNegative Prompt: {negative_prompt[:384]}\n\nStep: {kwargs.get('num_inference_steps')}, CFG: {kwargs.get('guidance_scale')}, CLIP Skip: {kwargs.get('clip_skip')}\nSampler: {pipeline.scheduler.config._class_name}\nLoRa: {pipeline.lora_dict}\nSeed: {item}\n\nModel:{pipeline.model_name}"
-        threading.Thread(target=lambda: pipeline.send_PIL_photo(image,file_name="Colab.PNG",file_type="PNG",caption=caption)).start()
+        threading.Thread(target=lambda: pipeline.send_PIL_photo(image,file_name=f"{pipeline.__class__.__name__}.PNG",file_type="PNG",caption=caption)).start()
         torch.cuda.empty_cache();gc.collect()
     return images
 
