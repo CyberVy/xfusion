8b7f37b03eb0b209e09529865d5fed34a9be30c5
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-20 21:56:45 +0800

    Update stable_diffusion_ui.py

--- xfusion/ui/stable_diffusion_ui.py
+++ xfusion/ui/stable_diffusion_ui.py
@@ -19,9 +19,9 @@ def load_stable_diffusion_ui(fns):
                 t2i_inputs.append(gr.Slider(0, 10, 1, step=0, label="CLIP Skip"))
                 t2i_inputs.append(gr.Slider(512, 2048, 1024, step=8, label="Width"))
                 t2i_inputs.append(gr.Slider(512, 2048, 1024, step=8, label="Height"))
-                t2i_inputs.append(gr.Textbox(value="0",placeholder="Give me an integer.", label="Seed"))
-                t2i_inputs.append(gr.Textbox(value="1",placeholder="Amount of the pictures.", label="Num"))
             with gr.Column():
+                t2i_inputs.append(gr.Textbox(value="0", placeholder="Give me an integer.", label="Seed"))
+                t2i_inputs.append(gr.Textbox(value="1", placeholder="Amount of the pictures.", label="Num"))
                 t2i_outputs.append(gr.Textbox())
                 t2i_btn = gr.Button("Run")
                 t2i_btn.click(fn=text_to_image_fn, inputs=t2i_inputs, outputs=t2i_outputs)
@@ -39,9 +39,9 @@ def load_stable_diffusion_ui(fns):
                 i2i_inputs.append(gr.Slider(0, 10, 3, step=0.1, label="Guidance Scale"))
                 i2i_inputs.append(gr.Slider(0, 50, 28, step=1, label="Step Skip"))
                 i2i_inputs.append(gr.Slider(0, 10, 0, step=1, label="CLIP"))
-                i2i_inputs.append(gr.Textbox(value="0",placeholder="Give me an integer.", label="Seed"))
-                i2i_inputs.append(gr.Textbox(value="1",placeholder="Amount of the pictures.", label="Num"))
             with gr.Column():
+                i2i_inputs.append(gr.Textbox(value="0", placeholder="Give me an integer.", label="Seed"))
+                i2i_inputs.append(gr.Textbox(value="1", placeholder="Amount of the pictures.", label="Num"))
                 i2i_outputs.append(gr.Textbox())
                 i2i_btn = gr.Button("Run")
                 i2i_btn.click(fn=image_to_image_fn, inputs=i2i_inputs, outputs=i2i_outputs)
