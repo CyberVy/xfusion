c5e750be0f19c924ebc19ee6899bbae3f3d015cf
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-21 02:10:26 +0800

    Update stable_diffusion_ui.py

--- xfusion/ui/stable_diffusion_ui.py
+++ xfusion/ui/stable_diffusion_ui.py
@@ -16,7 +16,7 @@ def load_stable_diffusion_ui(fns):
             with gr.Column():
                 t2i_inputs.append(gr.Slider(0,10,2.5,step=0.1,label="Guidance Scale"))
                 t2i_inputs.append(gr.Slider(0,50,28,step=1,label="Step"))
-                t2i_inputs.append(gr.Slider(0, 10, 1, step=0, label="CLIP Skip"))
+                t2i_inputs.append(gr.Slider(0, 10, 0, step=1, label="CLIP Skip"))
                 t2i_inputs.append(gr.Slider(512, 2048, 1024, step=8, label="Width"))
                 t2i_inputs.append(gr.Slider(512, 2048, 1024, step=8, label="Height"))
             with gr.Column():
@@ -37,8 +37,8 @@ def load_stable_diffusion_ui(fns):
             with gr.Column():
                 i2i_inputs.append(gr.Slider(0, 1, 0.3, step=0.1, label="Strength"))
                 i2i_inputs.append(gr.Slider(0, 10, 3, step=0.1, label="Guidance Scale"))
-                i2i_inputs.append(gr.Slider(0, 50, 28, step=1, label="Step Skip"))
-                i2i_inputs.append(gr.Slider(0, 10, 0, step=1, label="CLIP"))
+                i2i_inputs.append(gr.Slider(0, 50, 28, step=1, label="Step"))
+                i2i_inputs.append(gr.Slider(0, 10, 0, step=1, label="CLIP Skip"))
             with gr.Column():
                 i2i_inputs.append(gr.Textbox(value="0", placeholder="Give me an integer.", label="Seed"))
                 i2i_inputs.append(gr.Textbox(value="1", placeholder="Amount of the pictures.", label="Num"))
