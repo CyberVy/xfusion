c2a7a63e397ab1a11542de19baf54bb0c72831a5
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-21 23:51:34 +0800

    Update __init__.py

--- xfusion/enhancement/__init__.py
+++ xfusion/enhancement/__init__.py
@@ -9,22 +9,27 @@ def load_enhancer(pipeline_or_model=None,**kwargs):
         if isinstance(pipeline_or_model,str):
             if model_version in ["sdxl","pony"]:
                 model_version = "xl"
-            pipeline = SDPipelineEnhancer.from_url(pipeline_or_model,model_version=model_version,**kwargs)
+            enhancer = SDPipelineEnhancer.from_url(pipeline_or_model,model_version=model_version,**kwargs)
         else:
             pipeline = pipeline_or_model
-        enhancer = SDPipelineEnhancer(pipeline)
+            enhancer = SDPipelineEnhancer(pipeline)
         enhancer.model_name = pipeline_or_model
         download_kwargs = kwargs.get("download_kwargs")
         if isinstance(download_kwargs, dict):
             enhancer.set_download_kwargs(**download_kwargs)
         return enhancer
+
     elif model_version in ["flux"]:
         if isinstance(pipeline_or_model, str):
-            pipeline = FluxPipelineEnhancer.from_url(pipeline_or_model, **kwargs)
+            enhancer = FluxPipelineEnhancer.from_url(pipeline_or_model, **kwargs)
         else:
             pipeline = pipeline_or_model
-        enhancer = FluxPipelineEnhancer(pipeline)
+            enhancer = FluxPipelineEnhancer(pipeline)
         enhancer.model_name = pipeline_or_model
+        download_kwargs = kwargs.get("download_kwargs")
+        if isinstance(download_kwargs, dict):
+            enhancer.set_download_kwargs(**download_kwargs)
         return enhancer
+
     else:
         raise ValueError(f"{model_version} is not supported yet.")
