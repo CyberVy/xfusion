af3160b6a6eadab242d8bb4b769113ff7c50a70d
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-22 22:40:30 +0800

    Update ui_utils.py

--- xfusion/ui/ui_utils.py
+++ xfusion/ui/ui_utils.py
@@ -1,5 +1,5 @@
 class UIMixin:
     overrides = ["load_ui"]
 
-    def load_ui(self):
+    def load_ui(self,**kwargs):
         raise NotImplementedError(f"{object.__getattribute__(self, '__class__')} not implement 'load_ui'")
