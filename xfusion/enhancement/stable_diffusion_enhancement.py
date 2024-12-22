c68ce6b22944dacb03bc5028033e06e54d00ccb6
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-22 20:34:24 +0800

    Update stable_diffusion_enhancement.py

--- xfusion/enhancement/stable_diffusion_enhancement.py
+++ xfusion/enhancement/stable_diffusion_enhancement.py
@@ -1,185 +1,198 @@
-from ..utils import EasyInitSubclass,delete
-from ..ui.ui_utils import UIMixin
-from ..download import DownloadArgumentsMixin,download_file
-from ..message import TGBotMixin
-from diffusers.schedulers import DPMSolverMultistepScheduler,DPMSolverSinglestepScheduler
-from diffusers.schedulers import KDPM2DiscreteScheduler,KDPM2AncestralDiscreteScheduler
-from diffusers.schedulers import EulerDiscreteScheduler,EulerAncestralDiscreteScheduler
-from diffusers.schedulers import HeunDiscreteScheduler
-from diffusers.schedulers import LMSDiscreteScheduler
-from diffusers.schedulers import DEISMultistepScheduler
-from diffusers.schedulers import UniPCMultistepScheduler
-from diffusers import FluxPipeline,FluxImg2ImgPipeline,FluxInpaintPipeline
-from diffusers import StableDiffusionPipeline,StableDiffusionImg2ImgPipeline,StableDiffusionInpaintPipeline
-from diffusers import StableDiffusionXLPipeline,StableDiffusionXLImg2ImgPipeline,StableDiffusionXLInpaintPipeline
-from diffusers import StableDiffusion3Pipeline,StableDiffusion3Img2ImgPipeline,StableDiffusion3InpaintPipeline
+# limited support to sd3 now
 
-# from https://huggingface.co/docs/diffusers/api/schedulers/overview
-scheduler_map = {
-            "DPM++ 2M": (DPMSolverMultistepScheduler, {}),
-            "DPM++ 2M KARRAS": (DPMSolverMultistepScheduler, {"use_karras_sigmas": True}),
-            "DPM++ 2M SDE": (DPMSolverMultistepScheduler, {"algorithm_type": "sde-dpmsolver++"}),
-            "DPM++ 2M SDE KARRAS": (DPMSolverMultistepScheduler, {"use_karras_sigmas": True, "algorithm_type": "sde-dpmsolver++"}),
-            "DPM++ 2S A": (DPMSolverSinglestepScheduler, {}),
-            "DPM++ 2S A KARRAS": (DPMSolverSinglestepScheduler, {"use_karras_sigmas": True}),
-            "DPM++ SDE": (DPMSolverSinglestepScheduler, {}),
-            "DPM++ SDE KARRAS": (DPMSolverSinglestepScheduler, {"use_karras_sigmas": True}),
-            "DPM2": (KDPM2DiscreteScheduler, {}),
-            "DPM2 KARRAS": (KDPM2DiscreteScheduler, {"use_karras_sigmas": True}),
-            "DPM2 A": (KDPM2AncestralDiscreteScheduler, {}),
-            "DPM2 A KARRAS": (KDPM2AncestralDiscreteScheduler, {"use_karras_sigmas": True}),
-            "EULER": (EulerDiscreteScheduler, {}),
-            "EULER A": (EulerAncestralDiscreteScheduler, {}),
-            "HEUN": (HeunDiscreteScheduler, {}),
-            "LMS": (LMSDiscreteScheduler, {}),
-            "LMS KARRAS": (LMSDiscreteScheduler, {"use_karras_sigmas": True}),
-            "DEIS": (DEISMultistepScheduler, {}),
-            "UNIPC": (UniPCMultistepScheduler, {}),
-        }
-
-# pipeline_type 0,1,2 -> text_to_image, image_to_image, inpainting
-pipeline_map = {
-    "1.5":(StableDiffusionPipeline,StableDiffusionImg2ImgPipeline,StableDiffusionInpaintPipeline),
-    "xl":(StableDiffusionXLPipeline,StableDiffusionXLImg2ImgPipeline,StableDiffusionXLInpaintPipeline),
-    "3":(StableDiffusion3Pipeline,StableDiffusion3Img2ImgPipeline,StableDiffusion3InpaintPipeline),
-    "flux": (FluxPipeline,FluxImg2ImgPipeline,FluxInpaintPipeline)
-}
-
-class FromURLMixin:
-    overrides = ["from_url"]
-
-    @classmethod
-    def from_url(cls,url,init_sub_pipelines=True,**kwargs):
-        raise NotImplementedError(f"{cls} not implement 'from_url'")
+from .enhancement_utils import PipelineEnhancerBase,pipeline_map
+from ..components.component_utils import get_tokenizers_and_text_encoders_from_pipeline
+from ..components import load_stable_diffusion_pipeline
+from ..ui.stable_diffusion_ui import load_stable_diffusion_ui
+from ..utils import image_normalize,allow_return_error
+from compel import Compel,ReturnedEmbeddingsType
+import torch
+from PIL import Image
+import threading
+from random import randint
 
 
-def load_lora(pipeline,lora_uri,lora_name,download_kwargs=None):
+def get_embeds_from_pipeline(pipeline,prompt,negative_prompt):
+    """
+    To overcome 77 tokens limit, and support high level syntax for prompts.
+    limited support for sd1 sd2 sdxl
 
-    use_internet = True
-    if lora_uri.startswith(".") or lora_uri.startswith("/") or lora_uri.startswith("~"):
-        use_internet = False
-    if download_kwargs is None:
-        download_kwargs = {}
-    if use_internet:
-        lora_path = download_file(lora_uri,**download_kwargs)
-        pipeline.load_lora_weights(lora_path,adapter_name=lora_name)
-    else:
-        pipeline.load_lora_weights(lora_uri,adapter_name=lora_name)
-class LoraEnhancerMixin(DownloadArgumentsMixin,EasyInitSubclass):
+    :param pipeline:
+    :param prompt:
+    :param negative_prompt:
+    :return:
+    """
+    tokenizers,text_encoders = get_tokenizers_and_text_encoders_from_pipeline(pipeline)
+    tokenizers = [tokenizer for tokenizer in tokenizers if tokenizer]
+    text_encoders = [text_encoder for text_encoder in text_encoders if text_encoder]
+    for cls in pipeline_map["1.5"]:
+        if isinstance(pipeline,cls):
+            compel = Compel(tokenizers,text_encoders,truncate_long_prompts=False)
+            conditioning = compel(prompt)
+            negative_conditioning = compel(negative_prompt)
+            [conditioning, negative_conditioning] = compel.pad_conditioning_tensors_to_same_length([conditioning, negative_conditioning])
+            return {"prompt_embeds":conditioning,"negative_prompt_embeds":negative_conditioning}
+    for cls in pipeline_map["xl"]:
+        if isinstance(pipeline, cls):
+            compel = Compel(tokenizers,text_encoders,returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,requires_pooled=[False,True],truncate_long_prompts=False)
+            conditioning,pooled = compel(prompt)
+            negative_conditioning, negative_pooled = compel(negative_prompt)
+            [conditioning, negative_conditioning] = compel.pad_conditioning_tensors_to_same_length([conditioning, negative_conditioning])
+            return {"prompt_embeds":conditioning,"negative_prompt_embeds":negative_conditioning,
+                    "pooled_prompt_embeds":pooled,"negative_pooled_prompt_embeds":negative_pooled}
+    # compel only supports sd1 sd2 sdxl now
+    return {}
+class SDCLIPEnhancerMixin:
     # __oins__ here is the pipeline instance to implement.
     __oins__ = None
-    overrides = ["lora_dict","set_lora","set_lora_strength","delete_adapters"]
+    overrides = ["tokenizers","text_encoders","skip_clip_layer","get_embeds_from_pipeline","__call__"]
 
     def __init__(self):
-        DownloadArgumentsMixin.__init__(self)
-        self.lora_dict = {}
-        self.download_kwargs.update(directory="./lora")
+        self.tokenizers, self.text_encoders = get_tokenizers_and_text_encoders_from_pipeline(self)
 
-    def set_lora(self,lora_uri,lora_name,weight=0.4):
-        if lora_name not in self.lora_dict:
-            load_lora(self,lora_uri,lora_name,self.download_kwargs)
+    def skip_clip_layer(self,n):
+        text_encoders = [text_encoder for text_encoder in self.text_encoders if text_encoder]
+        if isinstance(n,int) and  n >= 1:
+            for text_encoder in text_encoders:
+                if not hasattr(text_encoder.text_model.encoder,"backup_layers"):
+                    text_encoder.text_model.encoder.backup_layers = text_encoder.text_model.encoder.layers
+                text_encoder.text_model.encoder.layers = text_encoder.text_model.encoder.backup_layers[:-n]
+        elif n is None or n == 0:
+            for text_encoder in text_encoders:
+                if hasattr(text_encoder.text_model.encoder,"backup_layers"):
+                    text_encoder.text_model.encoder.layers = text_encoder.text_model.encoder.backup_layers
+        else:
+            raise ValueError("An integer >=0 is required.")
 
-        self.set_lora_strength(lora_name,weight)
+    def get_embeds_from_pipeline(self,prompt,negative_prompt,clip_skip=None):
+        self.skip_clip_layer(clip_skip)
+        r =  get_embeds_from_pipeline(self,prompt,negative_prompt)
+        self.skip_clip_layer(0)
+        return r
 
-    def set_lora_strength(self,lora_name,weight):
-        self.lora_dict.update({lora_name:weight})
-        self.set_adapters(list(self.lora_dict.keys()),list(self.lora_dict.values()))
+    def __call__(self,**kwargs):
+        kwargs.update(self.get_embeds_from_pipeline(kwargs.get("prompt"),kwargs.get("negative_prompt"),kwargs.get("clip_skip")))
+        if kwargs.get("prompt_embeds") is not None:
+            kwargs.update(prompt=None,negative_prompt=None)
+        return self.__oins__.__call__(**kwargs)
 
-    def delete_adapters(self,adapter_names):
-        self.__oins__.delete_adapters(adapter_names)
-        if isinstance(adapter_names, str):
-            adapter_names = [adapter_names]
-        for name in adapter_names:
-            self.lora_dict.pop(name)
-
-
-class PipelineEnhancerBase(LoraEnhancerMixin,TGBotMixin,FromURLMixin,UIMixin,EasyInitSubclass):
-    overrides = ["enhancer_class","model_version","pipeline_type","pipeline_class",
-                 "model_name","_scheduler","scheduler_map","sub_pipelines",
-                 "image_to_image_pipeline","inpainting_pipeline",
-                 "check_original_pipeline","set_scheduler","reset_scheduler",
-                 "to",
-                 "clear","reload"]
+def generate_image_and_send_to_telegram(pipeline,prompt,negative_prompt,num,seed=None,use_enhancer=True,**kwargs):
+    kwargs.update(prompt=prompt,negative_prompt=negative_prompt)
+    seeds = []
+    images = []
+    if seed:
+        seeds.append(seed)
+    else:
+        for i in range(num):
+            seeds.append(randint(-2 ** 63, 2 ** 64 - 1))
+    for item in seeds:
+        kwargs.update(generator=torch.Generator(pipeline.device).manual_seed(item))
+        image = pipeline(**kwargs).images[0] if use_enhancer else pipeline.__oins__(**kwargs).images[0]
+        images.append(image)
+        caption = f"Prompt: {prompt[:384]}\n\nNegative Prompt: {negative_prompt[:384]}\n\nStep: {kwargs.get('num_inference_steps')}, CFG: {kwargs.get('guidance_scale')}, CLIP Skip: {kwargs.get('clip_skip')}\nSampler: {pipeline.scheduler.config._class_name}\nLoRa: {pipeline.lora_dict}\nSeed: {item}\n\nModel:{pipeline.model_name}"
+        threading.Thread(target=lambda: pipeline.send_PIL_photo(image,file_name=f"{pipeline.__class__.__name__}.PNG",file_type="PNG",caption=caption)).start()
+    return images
+class SDPipelineEnhancer(SDCLIPEnhancerMixin,PipelineEnhancerBase):
+    overrides = []
 
     def __init__(self,__oins__,init_sub_pipelines=True):
-        EasyInitSubclass.__init__(self,__oins__)
-        TGBotMixin.__init__(self)
-        LoraEnhancerMixin.__init__(self)
-        self.enhancer_class = object.__getattribute__(self,"__class__")
-        # pipeline_type 0,1,2 -> text_to_image, image_to_image, inpainting
-        self.model_version,self.pipeline_type,self.pipeline_class = self.check_original_pipeline()
-        self.model_name = self.name_or_path
-        self._scheduler = self.scheduler
-        self.scheduler_map = scheduler_map
+        PipelineEnhancerBase.__init__(self, __oins__,init_sub_pipelines=init_sub_pipelines)
+        SDCLIPEnhancerMixin.__init__(self)
 
-        self.sub_pipelines = []
-        if init_sub_pipelines:
-            if self.pipeline_type != 0:
-                self.text_to_image_pipeline = self.enhancer_class(pipeline_map[self.model_version][0](**self.components),
-                                                                  init_sub_pipelines=False)
-                self.text_to_image_pipeline.telegram_kwargs = self.telegram_kwargs
-                self.text_to_image_pipeline.lora_dict = self.lora_dict
-                self.text_to_image_pipeline.download_kwargs = self.download_kwargs
-                self.sub_pipelines.append(self.text_to_image_pipeline)
-            else:
-                self.text_to_image_pipeline = self
-
-            if self.pipeline_type != 1:
-                self.image_to_image_pipeline = self.enhancer_class(pipeline_map[self.model_version][1](**self.components),
-                                                                   init_sub_pipelines=False)
-                self.image_to_image_pipeline.telegram_kwargs = self.telegram_kwargs
-                self.image_to_image_pipeline.lora_dict = self.lora_dict
-                self.image_to_image_pipeline.download_kwargs = self.download_kwargs
-                self.sub_pipelines.append(self.image_to_image_pipeline)
-            else:
-                self.image_to_image_pipeline = self
-
-            if self.pipeline_type != 2:
-                self.inpainting_pipeline =  self.enhancer_class(pipeline_map[self.model_version][2](**self.components),
-                                                                init_sub_pipelines=False)
-                self.inpainting_pipeline.telegram_kwargs = self.telegram_kwargs
-                self.inpainting_pipeline.lora_dict = self.lora_dict
-                self.inpainting_pipeline.download_kwargs = self.download_kwargs
-                self.sub_pipelines.append(self.inpainting_pipeline)
-            else:
-                self.inpainting_pipeline = self
-
-    def check_original_pipeline(self):
-        for model_version, pipeline_class_tuple in pipeline_map.items():
-            for pipeline_type,pipeline_class in enumerate(pipeline_class_tuple):
-                if pipeline_class == self.__oinstype__:
-                    return model_version,pipeline_type,pipeline_class
-        raise TypeError(f"{self.__oinstype__} is not supported yet.")
-
-    def set_scheduler(self,scheduler_type,**kwargs):
-        if not isinstance(scheduler_type,str):
-            self.scheduler = scheduler_type.from_config(self.scheduler.config,**kwargs)
+    def __call__(self,**kwargs):
+        if kwargs.get("negative_prompt") is None:
+            kwargs.update(negative_prompt="")
+        prompt = kwargs.get("prompt")
+        negative_prompt = kwargs.get("negative_prompt")
+        if isinstance(prompt,list) and isinstance(negative_prompt,list):
+            prompt_type = list
+        elif isinstance(prompt,list) and not isinstance(negative_prompt,list):
+            negative_prompt = [negative_prompt for _ in range(len(prompt))]
+            prompt_type = list
+        elif not isinstance(prompt,list) and isinstance(negative_prompt,list):
+            prompt = [prompt for _ in range(len(negative_prompt))]
+            prompt_type = list
+        elif isinstance(prompt,str) and isinstance(negative_prompt,str):
+            prompt_type = str
         else:
-            if scheduler_type.upper() in self.scheduler_map:
-                self.scheduler = self.scheduler_map[scheduler_type.upper()][0].from_config(self.scheduler.config,**self.scheduler_map[scheduler_type.upper()][1])
-            else:
-                print(f"{scheduler_type} is not supported yet.")
+            raise ValueError("The type of prompt and negative_prompt need to be str or list.")
 
-    def reset_scheduler(self):
-        self.scheduler = self._scheduler
+        image = kwargs.get("image")
+        if image and isinstance(image,Image.Image):
+            kwargs.update(image=image_normalize(image,1024 * 1536))
 
-    def to(self, *args, **kwargs):
-        self.__oins__ = self.__oins__.to(*args, **kwargs)
-        return self
+        mask_image = kwargs.get("mask_image")
+        if mask_image and isinstance(mask_image, Image.Image):
+            kwargs.update(mask_image=image_normalize(image, 1024 * 1536))
 
-    def clear(self):
-        for component in self.components.values():
-            delete(component)
+        prompt_str = f"{prompt} {negative_prompt}" if prompt_type == str else f"{' '.join(prompt)} {' '.join(negative_prompt)}"
+        saved_lora_dict = self.lora_dict.copy()
 
-    def reload(self,url,**kwargs):
-        if not (url.startswith("http") or url.startswith(".") or url.startswith("/") or url.startswith("~")):
-            raise ValueError("A url is required.")
-        supported_model_version_list = [None,"","sdxl","xl","pony","1.5","2","3","3.5"]
-        if kwargs.get("model_version") not in supported_model_version_list:
-            raise ValueError(f"Model version: {kwargs.get("model_version")} is not supported, ${supported_model_version_list[2:]} is expected.")
+        for lora,weight in self.lora_dict.items():
+            if lora not in prompt_str:
+                self.set_lora_strength(lora, 0)
+                print(f"LoRA {lora}:{weight} is disable due to {lora} is not in prompts.")
+        try:
+            r = SDCLIPEnhancerMixin.__call__(self,**kwargs)
+        except Exception as e:
+            raise e
+        finally:
+            for lora,weight in saved_lora_dict.items():
+                self.set_lora_strength(lora,weight)
+        return r
 
-        device = self.device
-        self.clear()
-        object.__getattribute__(self,"__init__")(self.from_url(url,init_sub_pipelines=False,**kwargs).__oins__)
-        self.model_name = url
-        self.to(device)
+    def generate_image_and_send_to_telegram(self,
+                                            prompt,negative_prompt="",
+                                            guidance_scale=2,num_inference_steps=28,clip_skip=0,
+                                            width=None,height=None,
+                                            seed=None,num=1,
+                                            use_enhancer=True,**kwargs):
+        return generate_image_and_send_to_telegram(
+               self,
+               prompt=prompt,negative_prompt=negative_prompt,
+               guidance_scale=guidance_scale,num_inference_steps=num_inference_steps,clip_skip=clip_skip,
+               width=width,height=height,
+               seed=seed,num=num,
+               use_enhancer=use_enhancer,**kwargs)
+
+    @classmethod
+    def from_url(cls,url=None,model_version=None,init_sub_pipelines=True,**kwargs):
+        return cls(load_stable_diffusion_pipeline(model=url,model_version=model_version,**kwargs),
+                   init_sub_pipelines=init_sub_pipelines)
+
+    def load_ui(self,*args,**kwargs):
+        @allow_return_error
+        def model_selection(model,model_version):
+            self.reload(model,model_version=model_version)
+            return f"{model}, {model_version}"
+
+        @allow_return_error
+        def text_to_image(
+                   prompt, negative_prompt="",
+                   guidance_scale=2, num_inference_steps=28, clip_skip=0,
+                   width=None, height=None,
+                   seed=None, num=1):
+            return self.text_to_image_pipeline.generate_image_and_send_to_telegram(
+                   prompt=prompt,negative_prompt=negative_prompt,
+                   guidance_scale=guidance_scale,num_inference_steps=num_inference_steps,clip_skip=clip_skip,
+                   width=width,height=height,
+                   seed=int(seed),num=int(num))
+
+        @allow_return_error
+        def image_to_image(
+                   image,
+                   prompt,negative_prompt="",
+                   strength=0.3,
+                   guidance_scale=2,num_inference_steps=28,clip_skip=0,
+                   seed=None,num=1):
+            image = Image.fromarray(image)
+            return self.image_to_image_pipeline.generate_image_and_send_to_telegram(
+                   image=image,
+                   prompt=prompt,negative_prompt=negative_prompt,
+                   strength=strength,
+                   guidance_scale=guidance_scale,num_inference_steps=num_inference_steps,clip_skip=clip_skip,
+                   seed=int(seed),num=int(num))
+
+        server = load_stable_diffusion_ui(fns={"model_selection":model_selection,"text_to_image":text_to_image,"image_to_image":image_to_image})
+        server.launch(*args,**kwargs)
+        return server
