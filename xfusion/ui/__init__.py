cf5e4bb2f5e0e3cc6ef8edbf7d7e56e40d92c333
CyberVy <72680847+CyberVy@users.noreply.github.com>
2024-12-29 23:04:24 +0800

    Update __init__.py

--- xfusion/ui/__init__.py
+++ xfusion/ui/__init__.py
@@ -1,1 +1,2 @@
-from .stable_diffusion_ui import load_stable_diffusion_ui
+from .stable_diffusion_ui import load_stable_diffusion_ui,load_stable_diffusion_ui_for_multiple_pipelines
+from .flux_ui import load_flux_ui
