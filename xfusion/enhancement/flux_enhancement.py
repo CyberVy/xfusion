cc30027b2fd3f98e8369714706ebdcc23bc44add
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-05 01:30:41 +0800

    Update flux_enhancement.py

--- xfusion/enhancement/flux_enhancement.py
+++ xfusion/enhancement/flux_enhancement.py
@@ -1,7 +1,7 @@
 from .enhancement_utils import PipelineEnhancerBase
 from ..components.flux_components import load_flux_pipeline
 from ..ui.flux_ui import load_flux_ui
-from ..utils import image_normalize
+from ..utils import image_normalize,dict_to_str
 from PIL import Image
 import torch
 import threading
@@ -21,11 +21,15 @@ def generate_image_and_send_to_telegram(pipeline,prompt,num,seed=None,use_enhanc
         kwargs.update(generator=torch.Generator(pipeline.device).manual_seed(item))
         image = pipeline(**kwargs).images[0] if use_enhancer else pipeline.__oins__(**kwargs).images[0]
         images.append(image)
-        caption = f"Prompt:\n{prompt[:768]}\n\nStep: {kwargs.get('num_inference_steps')}, CFG: {kwargs.get('guidance_scale')}, CLIP Skip: {kwargs.get('clip_skip')}\nSampler: {pipeline.scheduler.config._class_name}\nSeed: {item}\n\nModel:{pipeline.model_name}"
+        
+        kwargs_for_telegram = kwargs.copy()
+        kwargs_for_telegram.update(prompt=f"\n{kwargs['prompt'][:768]}")
+        kwargs_for_telegram.pop("generator",None)
+        caption = dict_to_str(kwargs_for_telegram)
+        
+        caption += f"Sampler: {pipeline.scheduler.config._class_name}\nLoRa: {pipeline.lora_dict}\nSeed: {item}\n\nModel:{pipeline.model_name}"
         threading.Thread(target=lambda: pipeline.send_PIL_photo(image,file_name=f"{pipeline.__class__.__name__}.PNG",file_type="PNG",caption=caption)).start()
     return images
-
-
 class FluxPipelineEnhancer(PipelineEnhancerBase):
     overrides = []
 
@@ -43,7 +47,7 @@ class FluxPipelineEnhancer(PipelineEnhancerBase):
             kwargs.update(mask_image=mask_image)
             kwargs.update(width=mask_image.width)
             kwargs.update(height=mask_image.height)
-            
+
         return self.__oins__.__call__(*args,**kwargs)
 
     def generate_image_and_send_to_telegram(self,
