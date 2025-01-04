0cabd8157645f2930247bb0b09a2cccb0a284adc
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-04 18:59:51 +0800

    Update stable_diffusion_enhancement.py

--- xfusion/enhancement/stable_diffusion_enhancement.py
+++ xfusion/enhancement/stable_diffusion_enhancement.py
@@ -140,7 +140,7 @@ class SDPipelineEnhancer(SDCLIPEnhancerMixin,PipelineEnhancerBase):
 
         mask_image = kwargs.get("mask_image")
         if mask_image and isinstance(mask_image, Image.Image):
-            kwargs.update(image=image_normalize(image,width * height))
+            kwargs.update(mask_image=image_normalize(mask_image,width * height))
 
         prompt_str = f"{prompt} {negative_prompt}" if prompt_type == str else f"{' '.join(prompt)} {' '.join(negative_prompt)}"
 
