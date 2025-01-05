309c4670f6e519c5820a0b88681dc14a0b55c1f4
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-05 19:23:32 +0800

    Update flux_enhancement.py

--- xfusion/enhancement/flux_enhancement.py
+++ xfusion/enhancement/flux_enhancement.py
@@ -1,7 +1,7 @@
 from .enhancement_utils import PipelineEnhancerBase
 from ..components.flux_components import load_flux_pipeline
 from ..ui.flux_ui import load_flux_ui
-from ..utils import image_normalize,dict_to_str
+from ..utils import normalize_image,dict_to_str
 from PIL import Image
 import torch
 import threading
@@ -21,12 +21,12 @@ def generate_image_and_send_to_telegram(pipeline,prompt,num,seed=None,use_enhanc
         kwargs.update(generator=torch.Generator(pipeline.device).manual_seed(item))
         image = pipeline(**kwargs).images[0] if use_enhancer else pipeline.__oins__(**kwargs).images[0]
         images.append(image)
-        
+
         kwargs_for_telegram = kwargs.copy()
         kwargs_for_telegram.update(prompt=f"\n{kwargs['prompt'][:768]}")
         kwargs_for_telegram.pop("generator",None)
         caption = dict_to_str(kwargs_for_telegram)
-        
+
         caption += f"Sampler: {pipeline.scheduler.config._class_name}\nLoRa: {pipeline.lora_dict}\nSeed: {item}\n\nModel:{pipeline.model_name}"
         threading.Thread(target=lambda: pipeline.send_PIL_photo(image,file_name=f"{pipeline.__class__.__name__}.PNG",file_type="PNG",caption=caption)).start()
     return images
@@ -36,18 +36,23 @@ class FluxPipelineEnhancer(PipelineEnhancerBase):
     def __init__(self,__oins__,init_sub_pipelines=True):
         PipelineEnhancerBase.__init__(self,__oins__,init_sub_pipelines=init_sub_pipelines)
 
-    def __call__(self, *args, **kwargs):
+    def check_inference_kwargs(self,kwargs):
+
         image = kwargs.get("image")
         if image and isinstance(image, Image.Image):
-            kwargs.update(image=image_normalize(image, 1024 * 1024))
+            kwargs.update(image=normalize_image(image, 1024 * 1024))
 
         mask_image = kwargs.get("mask_image")
         if mask_image and isinstance(mask_image, Image.Image):
-            mask_image = image_normalize(mask_image, 1024 * 1024)
+            mask_image = normalize_image(mask_image, 1024 * 1024)
             kwargs.update(mask_image=mask_image)
             kwargs.update(width=mask_image.width)
             kwargs.update(height=mask_image.height)
 
+        return kwargs
+
+    def __call__(self, *args, **kwargs):
+        kwargs = self.check_inference_kwargs(kwargs)
         return self.__oins__.__call__(*args,**kwargs)
 
     def generate_image_and_send_to_telegram(self,
