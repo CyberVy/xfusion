68d9ca5227aa3fff304c4c09f8744914393f3486
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-11 23:21:56 +0800

    Update ui_utils.py

--- xfusion/ui/ui_utils.py
+++ xfusion/ui/ui_utils.py
@@ -3,3 +3,8 @@ class UIMixin:
 
     def load_ui(self,**kwargs):
         raise NotImplementedError(f"{object.__getattribute__(self, '__class__')} not implement 'load_ui'")
+
+
+def lists_append(element,lists):
+    for _list in lists:
+        _list.append(element)
