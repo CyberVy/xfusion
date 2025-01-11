d6876d74c8ed6dff8d8d6d5f505f144f4e7edf77
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-12 01:29:19 +0800

    Update __init__.py

--- xfusion/__init__.py
+++ xfusion/__init__.py
@@ -1,5 +1,5 @@
 import os
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 from .components import load_stable_diffusion_pipeline,load_flux_pipeline
 from .enhancement import SDPipelineEnhancer,FluxPipelineEnhancer
