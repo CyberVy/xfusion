c5b78954cacbd909176ec59da2d7c4085de4e285
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-12 23:50:58 +0800

    Update message_const.py

--- xfusion/message/message_const.py
+++ xfusion/message/message_const.py
@@ -1,21 +1,8 @@
-import requests
-
-
 Telegram_Bot_API_URL_Prefix = "https://api.telegram.org"
 Proxy_URL_Prefix = "https://us.xsolutiontech.com"
 
-def get_origin():
-    import re
-    country = None
-    try:
-        text = requests.get("http://104.16.0.0/cdn-cgi/trace",timeout=5).text
-        search = re.search("loc=(.*)",text)
-        if search:
-            country = search[1]
-    except IOError:
-        pass
-    return country
 
+from ..const import LOCATION
 
-if get_origin() in ["CN",None]:
+if LOCATION in ["CN",None]:
     Telegram_Bot_API_URL_Prefix = f"{Proxy_URL_Prefix}/{Telegram_Bot_API_URL_Prefix}"
