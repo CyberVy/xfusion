90a7587a8b54046fa8c93716d45baeea42308570
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-14 23:53:13 +0800

    Update enhancement_utils.py

--- xfusion/enhancement/enhancement_utils.py
+++ xfusion/enhancement/enhancement_utils.py
@@ -136,6 +136,8 @@ class PipelineEnhancerBase(ControlnetEnhancerMixin,LoraEnhancerMixin,TGBotMixin,
             else:
                 self.inpainting_pipeline = self
 
+            self.sync_sub_pipelines_mixin_kwargs()
+
     def check_original_pipeline(self):
         for model_version, pipeline_class_tuple in self.pipeline_map.items():
             for pipeline_type,pipeline_class in enumerate(pipeline_class_tuple):
@@ -192,10 +194,9 @@ class PipelineEnhancerBase(ControlnetEnhancerMixin,LoraEnhancerMixin,TGBotMixin,
         self.clear()
         object.__getattribute__(self,"__init__")(
             self.from_url(url,init_sub_pipelines=False,download_kwargs=download_kwargs,**kwargs).__oins__)
-        self.download_kwargs = download_kwargs
-        self.telegram_kwargs = telegram_kwargs
-        self.lora_dict = lora_dict
-        self.sync_sub_pipelines_mixin_kwargs()
+        self.set_download_kwargs(**download_kwargs)
+        self.set_telegram_kwargs(**telegram_kwargs)
+        self.lora_dict.update(**lora_dict)
         self.model_name = url
         self.to(device)
 
@@ -211,14 +212,7 @@ class PipelineEnhancerBase(ControlnetEnhancerMixin,LoraEnhancerMixin,TGBotMixin,
         lora_dict = self.lora_dict
         object.__getattribute__(self,"__init__")(
             self.from_url(url,init_sub_pipelines=False,download_kwargs=download_kwargs,**kwargs).__oins__)
-        self.download_kwargs = download_kwargs
-        self.telegram_kwargs = telegram_kwargs
-        self.lora_dict = lora_dict
-        self.sync_sub_pipelines_mixin_kwargs()
+        self.set_download_kwargs(**download_kwargs)
+        self.set_telegram_kwargs(**telegram_kwargs)
+        self.lora_dict.update(**lora_dict)
         self.model_name = url
-        
-    def __getattribute__(self, item):
-        attr = super().__getattribute__(item)
-        if item.startswith("set") and item.endswith("kwargs"):
-            self.sync_sub_pipelines_mixin_kwargs()
-        return attr
