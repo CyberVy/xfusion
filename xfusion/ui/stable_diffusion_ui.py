62df4de49ec69d1dc0daf6d91c3effc785cc5fca
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-16 07:04:14 +0800

    Update stable_diffusion_ui.py

--- xfusion/ui/stable_diffusion_ui.py
+++ xfusion/ui/stable_diffusion_ui.py
@@ -328,7 +328,8 @@ def load_stable_diffusion_ui(pipelines, _globals=None):
         @functools.wraps(f)
         def wrapper(*args,**kwargs):
             print(str(kwargs.get("seed")))
-            if int(kwargs.get("seed")) != 0 or len(pipelines) == 1:
+            if kwargs.get("seed") != 0 or len(pipelines) == 1:
+                print(str(kwargs.get("seed")))
                 return f(*args,**kwargs)(pipelines[0])
             else:
                 threads_execute(f(*args,**kwargs),pipelines)
