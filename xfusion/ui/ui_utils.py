a6f39a4d0c6bf3e0123a21d2f2d2744b62600ef1
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-22 10:12:54 +0800

    Update ui_utils.py

--- xfusion/ui/ui_utils.py
+++ xfusion/ui/ui_utils.py
@@ -8,3 +8,21 @@ class UIMixin:
 def lists_append(element,lists):
     for _list in lists:
         _list.append(element)
+
+def lock(lock_state=None):
+    lock_state = lock_state if lock_state is not None else [False]
+
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args):
+            if lock_state[0]:
+                raise RuntimeError(f"Async and multiple threads are not allowed for {f.__name__}.")
+            try:
+                lock_state[0] = True
+                return f(*args)
+            finally:
+                lock_state[0] = False
+
+        return wrapper
+
+    return decorator
