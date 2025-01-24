ea82c0ac1b3e190a828e6488b0fc9a4308a6d4fb
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-24 10:38:54 +0800

    Update message_utils.py

--- xfusion/message/message_utils.py
+++ xfusion/message/message_utils.py
@@ -1,6 +1,7 @@
 from .message_const import TELEGRAM_BOT_API_URL_PREFIX
 import requests
 from io import BytesIO
+from PIL.Image import Image
 
 
 def send_message_to_telegram(text,parse_mode="HTML",tg_token=None,chat_id=None):
@@ -8,7 +9,7 @@ def send_message_to_telegram(text,parse_mode="HTML",tg_token=None,chat_id=None):
                       data={"method":"POST","chat_id":chat_id,"text":text,"parse_mode":parse_mode})
     return r
 
-def send_PIL_photo(image,**kwargs):
+def send_PIL_photo(image:Image,**kwargs):
     """
     :param image:
     :param kwargs:
@@ -19,27 +20,17 @@ def send_PIL_photo(image,**kwargs):
         chat_id: telegram account id
     :return:
     """
-    caption = ""
-    file_name = "file.PNG"
-    file_type = "PNG"
-    token = "8001790084:AAFNqWprWz7WUnco5fob6U0CMHwockkZY8M"
-    chat_id = "5143886367"
-    if "caption" in kwargs.keys():
-        caption = kwargs["caption"]
-    if "file_name" in kwargs.keys():
-        file_name = kwargs["file_name"]
-    if "file_type" in kwargs.keys():
-        file_type = kwargs["file_type"]
-    if "token" in kwargs.keys():
-        token = kwargs["token"]
-    if "chat_id" in kwargs.keys():
-        chat_id = kwargs["chat_id"]
+    caption = kwargs.get("caption") or ""
+    file_type = kwargs.get("file_type") or "JPG"
+    file_name = kwargs.get("file_name") or f"file.{file_type}"
+    token = kwargs.get("token") or "8001790084:AAFNqWprWz7WUnco5fob6U0CMHwockkZY8M"
+    chat_id = kwargs.get("chat_id") or "5143886367"
     image_byte_array = BytesIO()
     image.save(image_byte_array, format=file_type)
     image_byte_array.seek(0)
     r = requests.post(f"{TELEGRAM_BOT_API_URL_PREFIX}/bot{token}/sendDocument",
                       data={"chat_id": chat_id, "caption": caption},
-                      files={"document": (file_name,image_byte_array,"image/png")})
+                      files={"document": (file_name,image_byte_array,f"image/{file_type.lower()}")})
     return r
 
 
