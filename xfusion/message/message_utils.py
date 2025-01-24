fefd7205659e09e922b2e22f4d74e9a2cc8356e6
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-24 21:36:59 +0800

    Update message_utils.py

--- xfusion/message/message_utils.py
+++ xfusion/message/message_utils.py
@@ -4,12 +4,12 @@ from io import BytesIO
 from PIL.Image import Image
 
 
-def send_message_to_telegram(text,parse_mode="HTML",tg_token=None,chat_id=None):
-    r = requests.post(f"{TELEGRAM_BOT_API_URL_PREFIX}/bot{tg_token}/sendMessage",
-                      data={"method":"POST","chat_id":chat_id,"text":text,"parse_mode":parse_mode})
+def send_text(text,parse_mode="HTML",token=None,chat_id=None):
+    r = requests.post(f"{TELEGRAM_BOT_API_URL_PREFIX}/bot{token}/sendMessage",
+                      data={"chat_id":chat_id,"text":text,"parse_mode":parse_mode})
     return r
 
-def send_PIL_photo(image:Image,**kwargs):
+def send_pil_photo(image:Image, **kwargs):
     """
     :param image:
     :param kwargs:
@@ -25,17 +25,18 @@ def send_PIL_photo(image:Image,**kwargs):
     file_name = kwargs.get("file_name") or f"file.{'JPG' if file_type.upper() == 'JPEG' else file_type.upper()}"
     token = kwargs.get("token") or "8001790084:AAFNqWprWz7WUnco5fob6U0CMHwockkZY8M"
     chat_id = kwargs.get("chat_id") or "5143886367"
+    parse_mode = kwargs.get("parse_mode") or "HTML"
     image_byte_array = BytesIO()
     image.save(image_byte_array, format=file_type)
     image_byte_array.seek(0)
     r = requests.post(f"{TELEGRAM_BOT_API_URL_PREFIX}/bot{token}/sendDocument",
-                      data={"chat_id": chat_id, "caption": caption},
+                      data={"chat_id": chat_id, "caption": caption,"parse_mode":parse_mode},
                       files={"document": (file_name,image_byte_array,f"image/{file_type.lower()}")})
     return r
 
 
 class TGBotMixin:
-    overrides = ["telegram_kwargs","send_PIL_photo","set_telegram_kwargs","generate_image_and_send_to_telegram"]
+    overrides = ["telegram_kwargs","send_text","send_pil_photo","set_telegram_kwargs","generate_image_and_send_to_telegram"]
 
     def __init__(self):
         self.telegram_kwargs = {}
@@ -43,10 +44,13 @@ class TGBotMixin:
     def generate_image_and_send_to_telegram(self, **kwargs):
         raise NotImplementedError(f"{object.__getattribute__(self, '__class__')} not implement 'generate_image_and_send_to_telegram'")
 
-    def send_PIL_photo(self,image,**kwargs):
+    def send_pil_photo(self, image, **kwargs):
         kw = self.telegram_kwargs.copy()
         kw.update(**kwargs)
-        send_PIL_photo(image,**kw)
+        send_pil_photo(image, **kw)
+
+    def send_text(self,text):
+        send_text(text,**self.telegram_kwargs)
 
     def set_telegram_kwargs(self,**kwargs):
         self.telegram_kwargs.update(**kwargs)
