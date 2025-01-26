adcbdfe31afbb69a0862358a2c04b8bb537d3968
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-01-27 00:15:57 +0800

    Update stable_diffusion_ui.py

--- xfusion/ui/stable_diffusion_ui.py
+++ xfusion/ui/stable_diffusion_ui.py
@@ -352,7 +352,7 @@ def stable_diffusion_ui_template(fns):
             code_outputs = []
             with gr.Row():
                 with gr.Column():
-                    code_inputs.append(gr.Code(value="import os,sys,gc,torch\n_cout = 'Hello world.'", language="python", lines=5, label="Python"))
+                    code_inputs.append(gr.Code(value="import os,sys,gc,torch\nfrom diffusers.callbacks import *\n_cout = 'Hello world.'", language="python", lines=5, label="Python"))
                 with gr.Column():
                     code_outputs.append(gr.Textbox(label="Code Result"))
                     code_btn = gr.Button("Run Code")
