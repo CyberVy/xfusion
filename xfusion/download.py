7f0959f638bec6d953299f8664fb5596ef348dc5
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-02-19 01:42:29 +0800

    Update download.py

--- xfusion/download.py
+++ xfusion/download.py
@@ -25,8 +25,12 @@ if NEED_PROXY or 1:
     @functools.wraps(original_session_request)
     def request(method,url,*args,**kwargs):
         # a header with content-length is required, which is not supported by cloudflare.
-        if not url.startswith(PROXY_URL_PREFIX):
-            url = f"{PROXY_URL_PREFIX}/{url}"
+        
+        if not url.startswith(PROXY_URL_PREFIX) and not url.startswith('https://dhp.xsolutiontech.com'):
+            if method == "HEAD" :
+                url = f"{'https://dhp.xsolutiontech.com'}/{url}"
+            else:
+                url = f"{PROXY_URL_PREFIX}/{url}"
         print(url)
         return original_session_request(method,url,*args,**kwargs)
     session.request = request
