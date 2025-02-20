8db2cd36eedd92e0bd7ee7f65e2e0e29368166f5
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-02-20 20:29:13 +0800

    Update stable_diffusion_ui.py

--- xfusion/ui/stable_diffusion_ui.py
+++ xfusion/ui/stable_diffusion_ui.py
@@ -408,7 +408,7 @@ def render_code(fns):
 def render_stable_diffusion_ui(fns):
     theme = gr.themes.Ocean()
 
-    with gr.Blocks(title=f"Xfusion{GPU_NAME}", theme=theme) as server:
+    with gr.Blocks(title=f"SD{GPU_NAME}", theme=theme) as server:
 
         render_model_selection(fns)
         render_lora(fns)
