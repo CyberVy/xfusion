414fe8ef018936c529774cb85fc37c144fa42319
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-03-17 20:34:35 +0800

    Update stable_diffusion_enhancement.py

--- xfusion/enhancement/stable_diffusion_enhancement.py
+++ xfusion/enhancement/stable_diffusion_enhancement.py
@@ -141,7 +141,7 @@ def generate_image_and_send_to_telegram(pipeline,prompt,negative_prompt,num,seed
         images.append(image)
 
         kwargs_for_telegram = kwargs.copy()
-        kwargs_for_telegram.update(prompt=f"\n{kwargs['prompt'][:640]}")
+        kwargs_for_telegram.update(prompt=f"\n{kwargs['prompt'][:512]}")
         kwargs_for_telegram.update(negative_prompt=f"\n{kwargs['negative_prompt'][:128]}")
         kwargs_for_telegram.pop("generator",None)
         caption = dict_to_str(kwargs_for_telegram)
