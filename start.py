938db6a5ce854b0da78dd53b8c479059b6c903e2
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-08-03 15:46:51 +0800

    Update start.py

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ download_kwargs = {"directory":os.environ.get("DOWNLOAD_PATH","/xfusion_models")
 pipelines = [load_enhancer(None,model_version=os.environ.get("MODEL_VERSION",""),download_kwargs=download_kwargs,telegram_kwargs=telegram_kwargs) for i in range(GPU_COUNT)]
 
 if os.environ.get("MODEL_VERSION","") in ["", "1.5", "2", "3", "3.5", "xl", "sdxl", "pony"]:
-    print("hello")
+    print(telegram_kwargs)
     server = load_stable_diffusion_ui(pipelines,_globals=globals(),pwa=True)
 elif os.environ.get("MODEL_VERSION","") in ["flux"]:
     server = load_flux_ui(pipelines,_globals=globals(),pwa=True)
