04f8dceaed95a05a8ef3898c8f91085b328f3490
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-08-03 17:21:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,15 +4,3 @@ else
   pip install -q git+https://github.com/CyberVy/xfusion.git & transformers-cli env 2> /dev/null & wait
 fi
 
-
-for arg in "$@"; do
-  if [[ $arg == --env=* ]]; then
-    pair="${arg#--env=}"
-    key="${pair%%=*}"
-    value="${pair#*=}"
-    export "$key=$value"
-  fi
-done
-
-curl -Lso start.py "https://raw.githubusercontent.com/CyberVy/xfusion/refs/heads/main/start.py"
-python3 ./start.py
