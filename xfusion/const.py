5e2cc70a170217575991781b917527a5a5248a65
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-08-03 16:59:57 +0800

    Update const.py

--- xfusion/const.py
+++ xfusion/const.py
@@ -7,8 +7,8 @@ GPU_NAME = [torch.cuda.get_device_name(i) for i in range(GPU_COUNT)]
 XFUSION_COOKIE = os.environ.get("XFUSION_COOKIE")
 XFUSION_PROXY = os.environ.get("XFUSION_PROXY")
 
-HF_HUB_TOKEN = os.environ.get("HF_HUB_TOKEN") or "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"
-CIVITAI_TOKEN = os.environ.get("CIVITAI_TOKEN") or "1dab3490177fd9b4985323655f917d0c"
+HF_HUB_TOKEN = os.environ.get("HF_HUB_TOKEN")
+CIVITAI_TOKEN = os.environ.get("CIVITAI_TOKEN")
 
 PROXY_URL_PREFIX = os.environ.get("PROXY_URL_PREFIX") or "https://us2.xsolutiontech.com"
 
