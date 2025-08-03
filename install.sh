f9bca57a973ddca84483752d335a7446c84403d8
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-08-03 17:24:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-if python3 -m pip show xfusion > /dev/null 2>&1; then
+if pip show xfusion > /dev/null 2>&1; then
   echo "xfusion is installed."
 else
   pip install -q git+https://github.com/CyberVy/xfusion.git & transformers-cli env 2> /dev/null & wait
