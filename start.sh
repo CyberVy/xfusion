0b30a2b552fbe2a267ff1ba243d77f40c0705cc4
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-08-03 16:01:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
- pip install -q git+https://github.com/CyberVy/xfusion.git & transformers-cli env 2> /dev/null & wait
+if python3 -m pip show xfusion > /dev/null 2>&1; then
+  echo "xfusion is installed."
+else
+  pip install -q git+https://github.com/CyberVy/xfusion.git & transformers-cli env 2> /dev/null & wait
+fi
+
 
 for arg in "$@"; do
   if [[ $arg == --env=* ]]; then
@@ -9,5 +14,4 @@ for arg in "$@"; do
   fi
 done
 
-curl -Lo ./start.py "https://raw.githubusercontent.com/CyberVy/xfusion/refs/heads/main/start.py"
 python3 ./start.py
